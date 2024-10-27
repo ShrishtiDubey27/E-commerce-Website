0c9f9e3b8991558bb12029ed1f5adec619edbe81
ShrishtiDubey27 <shrishtidubey5204@gmail.com>
2024-10-27 13:01:20 +0530

    login authentication for admin and logout  feature added

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 import { Routes, Route } from "react-router-dom";
@@ -6,6 +6,8 @@ import Add from "./pages/Add";
 import List from "./pages/List";
 import Orders from "./pages/Orders";
 import Login from "./components/Login";
+import { ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export const backendUrl =import.meta.env.VITE_BACKEND_URL
 
@@ -13,24 +15,28 @@ export const backendUrl =import.meta.env.VITE_BACKEND_URL
 const App = () => {
 
 
-   const [token,setToken]=useState('');
+   const [token,setToken]=useState(localStorage.getItem('token')?localStorage.getItem('token'):'');
+
+   useEffect(()=>{
+localStorage.setItem('token',token)
+   },[token])
 
   return (
     <div className="bg-gray-50 min-h-screen">
-
+   <ToastContainer/>
       {token ===""
-      ?<Login/>:
+      ?<Login setToken={setToken}/>:
       <>
-      <Navbar></Navbar>
+      <Navbar setToken={setToken}></Navbar>
       <hr />
       <div className="flex w-full">
         <Sidebar></Sidebar>
 
         <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
           <Routes>
-            <Route path="/add" element={<Add />}></Route>
-            <Route path="/list" element={<List />}></Route>
-            <Route path="/orders" element={<Orders />}></Route>
+            <Route path="/add" element={<Add token={token}/>}></Route>
+            <Route path="/list" element={<List token={token}/>}></Route>
+            <Route path="/orders" element={<Orders token={token}/>}></Route>
           </Routes>
         </div>
       </div>
--- admin/src/components/Login.jsx
+++ admin/src/components/Login.jsx
@@ -1,9 +1,28 @@
 import React, { useState } from 'react'
 import axios from 'axios'
 import {backendUrl} from '../App'
+import { toast } from 'react-toastify'
 
-const Login = () => {
-  
+const Login = ({setToken}) => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const onSubmitHandler = async (e) => {
+    try {
+      e.preventDefault() 
+      const response=await axios.post(backendUrl+'/api/user/admin',{email,password})
+      if(response.data.success)
+      {
+        setToken(response.data.token)
+      }
+      else{
+        toast.error(response.data.message)
+      }
+    } catch (error) {
+     console.log(error)
+     toast.error(error.message)
+    }
+  }
 
   return (
     <div className='flex items-center justify-center h-screen w-full'>
