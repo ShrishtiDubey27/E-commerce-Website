d4bf0acbdb4eba6e0d97f8ffef27e5a36b973b50
ShrishtiDubey27 <shrishtidubey5204@gmail.com>
2024-10-30 23:00:29 +0530

    integrated Cart Api with frontend

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -65,6 +65,16 @@ const ShopContextProvider = (props) => {
     let cartData = structuredClone(cartItems);
     cartData[itemId][size] = quantity;
     setCartItems(cartData);
+
+    if(token)
+      {
+        try {
+          await axios.post(backendUrl +'/api/cart/update',{itemId, size,quantity},{headers:{token}})
+        } catch (error) {
+          console.log(error)
+          toast.error(error.message)
+        }
+      }
   };
 
   const getCartAmount = () => {
@@ -101,6 +111,22 @@ const ShopContextProvider = (props) => {
     }
   }
 
+
+  const getUserCart=async(token)=>{
+
+    try {
+      const response=await axios.post(backendUrl+'/api/cart/get',{},{headers:{token}})
+      if(response.data.success)
+      {
+        setCartItems(response.data.cartData)
+      }
+    } catch (error) {
+      console.log(error)
+      toast.error(error.message)
+    }
+
+  }
+
   useEffect(()=>{
    getProductsData()
   },[])
@@ -108,7 +134,8 @@ const ShopContextProvider = (props) => {
   useEffect(()=>{
    if(!token && localStorage.getItem('token'))
    {
-    setToken(localStorage.getItem('token'))
+    setToken(localStorage.getItem('token'));
+    getUserCart(localStorage.getItem('token'))
    }
   },[])
 
