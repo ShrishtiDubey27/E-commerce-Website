095df9bf1423bdf9c5213bd4ee5b72f6b3de7c05
ShrishtiDubey27 <shrishtidubey5204@gmail.com>
2024-11-02 08:18:43 +0530

    Orders API integrated with frontend

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -32,7 +32,16 @@ const placeOrderRazorpay = async (req, res) => {};
 
 const allOrders = async (req, res) => {};
 
-const userOrders = async (req, res) => {};
+const userOrders = async (req, res) => {
+  try {
+    const { userId } = req.body;
+    const orders = await orderModel.find({ userId });
+    res.json({ success: true, orders });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 // update using Admin Page only
 const updateStatus = async (req, res) => {};
